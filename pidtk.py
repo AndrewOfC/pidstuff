ddb63769371c6865e8ad89fa28c2b1f148a65ac3
ANDREW PAGE <andrew.e.page@gmail.com>
2020-11-08 12:00:16 -0500

    basic ui

--- pidtk.py
+++ pidtk.py
@@ -1,21 +1,66 @@
 
 
 from tkinter import *
+from simple_pid import PID
 
 
 class TkApp(object):
     def __init__(self):
+        
+        master = self._master = Tk()
+        
+        
+        self._pid = PID()
+        
+        self._runVar = BooleanVar(master, value=False)
+        
         return
+    
+    def _timer(self):
+        return
+    
+    def _onRun(self):
+        return 
     
     def run(self):
         
-        
-        master = Tk()
-        
+        master = self._master 
+       
         
         s = Scale(master, from_=-100, to=100,tickinterval=20, orient=HORIZONTAL)
         
         s.pack(fill='x', expand=1)
+        
+        ##
+        ## Run
+        ##
+        run_cb = Checkbutton(master, text="run", command=self._onRun, onvalue=True, offvalue=False, variable=self._runVar)
+        run_cb.pack()
+        
+        ##
+        ## Kp
+        ##
+        Label(master, text="Kp").pack()
+        kp_entry = Entry(master)
+        kp_entry.pack()
+        
+        ##
+        ## Ki
+        ##
+        Label(master, text="Ki").pack()
+        ki_entry = Entry(master)
+        ki_entry.pack()    
+        
+        ##
+        ## Kd
+        ##
+        Label(master, text="Kd").pack()
+        kd_entry = Entry(master)
+        kd_entry.pack()        
+        
+        apply = Button(master, text="Apply")
+        
+        apply.pack()
         
         mainloop()
         
